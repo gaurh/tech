70acc9aef9232a57dbbf753afb861c7f5347fea8
gaurh <31615980+gaurh@users.noreply.github.com>
2017-12-22 09:28:10 +0530

    update
    some more changes

--- 100.c
+++ 100.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int a,b,c;
-c=a;
-a=b;
-b=c;
-printf("swaped no's:%d%d",a,b);
+int a,b;
+a=a+b;
+b=a-b;
+a=a-b;
+printf("swaped no's a:%d",a);
+  printf("swaped no's b:%d",b);
 }
